9c3c607aabc6cb2f32c9ad6cb720a591b250a693
zzhhzt <32793665+zzhhzt@users.noreply.github.com>
2022-06-10 22:08:43 +0800

    Update macos-guest-virtualbox.sh

--- macos-guest-virtualbox.sh
+++ macos-guest-virtualbox.sh
@@ -20,9 +20,9 @@ macOS_release_name="Catalina"    # install "HighSierra" "Mojave" or "Catalina"
 storage_size=80000               # VM disk image size in MB, minimum 22000
 storage_format="vdi"             # VM disk image file format, "vdi" or "vmdk"
 cpu_count=2                      # VM CPU cores, minimum 2
-memory_size=4096                 # VM RAM in MB, minimum 2048
+memory_size=6096                 # VM RAM in MB, minimum 2048
 gpu_vram=128                     # VM video RAM in MB, minimum 34, maximum 128
-resolution="1280x800"            # VM display resolution
+resolution="1366x768"            # VM display resolution
 
 # Values for NVRAM and EFI parameters are required by iCloud, iMessage,
 # and other connected Apple applications, but otherwise not required.
